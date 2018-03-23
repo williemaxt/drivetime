44d4630db6f4bc6474f30dcb275caae6cad61a6a
cmerchant1598 <cmerchant1598@gmail.com>
2018-03-23 01:28:05 -0400

    Update driverRegister.php
    Added User Authentication. Changed connection type. Changed all variables for sql input.

--- driverRegister.php
+++ driverRegister.php
@@ -1,17 +1,34 @@
-<?php 
-include_once 'connection.php';
-    //inserting the form inputs as variables
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $number = $_POST['number'];
-    $medical = $_POST['medical'];
-    $crash_report = $_POST['crash_report'];
-    #this line above (line 6) is saying post the commit date variable into sql table
-    #this line below (line 8) is telling the commit date variable what the format is for both the date and time.
-    $sql = "INSERT INTO drivers (name, email, number, medical, crash_report) VALUES ('$name', '$email', '$number', '$medical', '$crash_report');";
-    mysqli_query($conn, $sql);
+<?php
+
+
+    if(isset($_POST['submit'])) {
+        $con = new mysqli('localhost','root','Encryptme!!!','drive_time');
+
+        $name = $con->real_escape_string($_POST['name']);
+        $email = $con->real_escape_string($_POST['email']);
+        $number = $con->real_escape_string($_POST['number']);
+        $password = $con->real_escape_string($_POST['password']);
+        $cPassword = $con->real_escape_string($_POST['cPassword']);
+        $medical = $con->real_escape_string($_POST['medical']);
+        $crash_report = $con->real_escape_string($_POST['crash_report']);
+
+        if ($password != $cPassword)
+        $msg = "Passwords do not match.";
+        else {
+            $hash = password_hash($password, PASSWORD_BCRYPT);
+            $con->query("INSERT INTO drivers (name, email, number, password, medical, crash_report) VALUES ('$name', '$email', '$number', '$hash', '$medical', '$crash_report')");
+            $msg = "You have been registered!";
+
+            //ADD DASHBOARD REDIRECT HERE...
+    }
+
+
+    //$sql = "INSERT INTO drivers (name, email, number, password, medical, crash_report) VALUES ('$name', '$email', '$number', '$password', '$medical', '$crash_report');";
+    //mysqli_query($conn, $sql);
     //take us back home after submission
-   // header("Location: allcode.php?commit=success");
+    // header("Location: allcode.php?commit=success");
+
+}
 ?>
 <!DOCTYPE html>
 <html>
@@ -23,21 +40,24 @@ include_once 'connection.php';
   </head>
   <body>
       <br>
+      <?php if ($msg != "")echo $msg . "<br><br>" ?>
       <form id="registerDriverForm" method="post" action="driverRegister.php">
           <h1>Register To Drive</h1>
           <p>Full Name</p>
-          <input type="text" name="name">
+          <input type="text" minlength="3" name="name">
           <p>Email</p>
           <input type="email" name="email">
           <p>Password</p>
-          <input type="password" name="password">
+          <input type="password" minlength="8" name="password">
+          <p>Confirm Password</p>
+          <input type="password" minlength="8" name="cPassword">
           <p>Phone</p>
           <input type="number" name="number">
           <p>Medical</p>
           <input type="file" name="medical">
           <p>Crash Report</p>
           <input type="file" name="crash_report">
-          <input class="submitBtn" type="submit" value="Submit">
+          <input class="submitBtn" type="submit" name="submit" value="Submit">
       </form>
 
   </body>
