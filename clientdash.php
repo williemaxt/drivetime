d418f4e11588eefc9d1fa5e714fafe3294fd98ae
William Thompson <williemaxt@gmail.com>
2018-03-25 00:50:27 -0400

    Create clientdash.php

---
+++ clientdash.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Drive Time</title>
+    <link rel="stylesheet" href="css/dash.css">
+  </head>
+  
+    <body>
+        
+        <nav>
+            <p><?php echo  $row['name'] ; ?></p>
+            <p>null</p>
+            <p>null</p>
+        </nav>
+    </body>
+</html>
