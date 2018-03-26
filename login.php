a826690dbd4324f85b115b47f59187c8cf4c6ab7
cmerchant1598 <cmerchant1598@gmail.com>
2018-03-26 13:03:47 -0400

    Update login.php
    This is client login. Everything is working correctly.

--- login.php
+++ login.php
@@ -1,22 +1,44 @@
-<!DOCTYPE html>
+<?php
+require_once("connection.php");
+if(isset($_POST['submit'])){
+    $email = trim($_POST['email']);
+    $password = trim($_POST['password']);
+
+    $sql = "select * from clients where email = '".$email."'";
+    $rs = mysqli_query($conn,$sql);
+    $numRows = mysqli_num_rows($rs);
+
+    if($numRows  == 1){
+        $row = mysqli_fetch_assoc($rs);
+        if(password_verify($password,$row['password'])){
+
+            header('Location: clientdash.php');
+        }
+        else{
+            echo "Either your email or password are incorrect. Please try again.";
+        }
+    }
+    else{
+        echo "No User found";
+    }
+}
+?>
 <html>
-  <head>
+<head>
     <meta charset="utf-8">
     <title>Reset Password</title>
-      <link rel="stylesheet" href="css/forms.css">
-  </head>
-  <body>
-      <?php include 'connection.php'?>
-      <br>
-      <form id="loginForm">
-          <h1>Login</h1>
-          <p>Email</p>
-          <input type="text" name="lgEmail">
-          <p>Password</p>
-          <input type="password" name="lgPassword">
-          <input class="submitBtn" type="submit" value="Login">
-          <a style="text-align:center;" href="index.html"><p>Forgot Password?</p></a>
-      </form>
-
-  </body>
+    <link rel="stylesheet" href="css/forms.css">
+</head>
+<body>
+<br>
+<form id="loginForm" action="<?php echo $_SERVER['PHP_SELF']?>" method="post">
+    <h1>Login</h1>
+    <p>Email</p>
+    <input type="text" name="email" value="">
+    <p>Password</p>
+    <input type="password" name="password" value="">
+    <button type="submit" name="submit">Submit</button>
+    <a style="text-align:center;" href="index.html"><p>Forgot Password?</p></a>
+</form>
+</body>
 </html>
