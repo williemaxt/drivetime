3c6dbc5b78e6f39f7100efb9f2e8f808fd1ce9f3
William Thompson <williemaxt@gmail.com>
2018-03-25 22:09:24 -0400

    Update clientdash.php
    This Update is the latest layout for the client dash **********DOES NOT yet have mobile compatibility***********

--- clientdash.php
+++ clientdash.php
@@ -1,3 +1,12 @@
+<?php
+      include_once 'connection.php';
+    //write an sql statement to pull all information from table
+   // $sql = "SELECT * FROM code;";
+      $sql  = 'SELECT * FROM `drivers`';
+    //variable to query the code
+    //conn istaken from the connection file
+    $result = mysqli_query($conn, $sql); 
+?>
 <!DOCTYPE html>
 <html>
   <head>
@@ -9,9 +18,71 @@
     <body>
         
         <nav>
-            <p><?php echo  $row['name'] ; ?></p>
-            <p>null</p>
-            <p>null</p>
+            <h1>Prodrivetime</h1>
+            <p><a href="#">Logout</a></p>
+            <p>example@gmail.com</p>
+            <p>John Doe</p>
         </nav>
+        <!--This is where the list of drivers will show-->
+        <div class="wrapper">
+        <main>
+        <h1>Hire A Pro</h1>
+          <!--An example of a driver card that will show-->
+            <div class="driver-card">
+            <p>NAME:</p>
+            <p>EXPERIENCE:</p>
+            <p>Bio:</p>
+            <p>Medical:</p>
+            <p>Crash Rep:</p>
+                <input type="submit" value="HIRE">
+            </div>
+            
+            <div class="driver-card">
+            <p>NAME:</p>
+            <p>EXPERIENCE:</p>
+            <p>Bio:</p>
+            <p>Medical:</p>
+            <p>Crash Rep:</p>
+                <input type="submit" value="HIRE">
+            </div>
+            
+            <div class="driver-card">
+            <p>NAME:</p>
+            <p>EXPERIENCE:</p>
+            <p>Bio:</p>
+            <p>Medical:</p>
+            <p>Crash Rep:</p>
+                <input type="submit" value="HIRE">
+            </div>
+            
+            <div class="driver-card">
+            <p>NAME:</p>
+            <p>EXPERIENCE:</p>
+            <p>Bio:</p>
+            <p>Medical:</p>
+            <p>Crash Rep:</p>
+                <input type="submit" value="HIRE">
+            </div>
+            
+            <div class="driver-card">
+            <p>NAME:</p>
+            <p>EXPERIENCE:</p>
+            <p>Bio:</p>
+            <p>Medical:</p>
+            <p>Crash Rep:</p>
+                <input type="submit" value="HIRE">
+            </div>
+          
+        </main>
+        <!--This section shows the clients account information-->
+        <aside>
+             <h1>My Account</h1>
+            <p>NAME:</p>
+            <p>EMAIL:</p>
+            <p>PHONE:</p>
+            <p>ADDRESS:</p>
+            
+        </aside>
+        </div>
     </body>
 </html>
