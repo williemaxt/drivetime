7c933fc94f9a1e2df7becef72c3174e7c2829e8f
William Thompson <williemaxt@gmail.com>
2018-03-31 23:53:36 -0400

    Update driverdash.php
    Now the dash board will pull from the database. And the next update will pull from the current user

--- driverdash.php
+++ driverdash.php
@@ -1,12 +1,13 @@
 <?php
       include_once 'connection.php';
     //write an sql statement to pull all information from table
-   // $sql = "SELECT * FROM code;";
       $sql  = 'SELECT * FROM `clients`';
+      //the query will look for users with that email and display their information on the dashboard
+      $sql1 = 'SELECT * FROM `drivers` WHERE email = "God@gmail.com"';
     //variable to query the code
-    //conn istaken from the connection file
-    $result = mysqli_query($conn, $sql); 
-    
+    //conn is taken from the connection file
+    $result = mysqli_query($conn, $sql);
+    $result1 = mysqli_query($conn, $sql1);
     session_start();
 	if(!isset($_SESSION))
     {
@@ -28,39 +29,53 @@
         <nav>
             <h1>Prodrivetime</h1>
             <p><a href="logout.php?logout=true">Logout</a></p>
-            <p>example@gmail.com</p>
-            <p>John Doe</p>
+            <!--the php snippet below echos the account information of the person logged in-->
+            <?php
+             if($result1->num_rows > 0){
+                 $row1 = $result1->fetch_assoc();
+                     echo '<p> ' . $row1['email'] . '</p>
+                     <p>' . $row1['name'] . '</p>';
+             }else{
+                 echo 'We cant seem to pull your info';
+             }
+
+            ?>
         </nav>
         <!--This is where the list of drivers will show-->
         <div class="wrapper">
         <!--This section shows the clients account information-->
         <aside>
              <h1>My Account</h1>
-            <p>NAME:</p>
-            <p>EMAIL:</p>
-            <p>PHONE:</p>
-            <p>ADDRESS:</p>
-            
+            <?php
+            if($result1->num_rows > 0){
+                echo '
+                <p>NAME: ' . $row1['name'] . '</p>
+                <p>EMAIL: ' . $row1['email'] . '</p>
+                <p>PHONE: ' . $row1['number'] . '</p>
+                <p>STATE: ' . $row1['state'] . '</p>';
+            }else{
+                echo 'We cant seem to pull your info';
+            }
+            ?>
         </aside>
             
         <main>
         <h1>Your Requests</h1>
             
-    <?php
-        if ($result->num_rows > 0) {
-        // output data of each row in the database and displays it as a card
-        while($row = $result->fetch_assoc()) {
-        echo '<div class="driver-card">
-            <p>NAME: ' . $row['name'] . '</p>
-            
-            <p>BUSINESSES: ' . $row['bname'] . '</p>
-            <p>ADDRESS: ' . $row['baddr'] . '</p>
-            <input type="submit" name="accept" value="ACCEPT">
-            </div>';
-        }
+            <?php
+                if ($result->num_rows > 0) {
+                 // output data of each row in the database and displays it as a card
+                while($row = $result->fetch_assoc()) {
+                echo '<div class="driver-card">
+                <p>NAME: ' . $row['name'] . '</p>
+                <p>BUSINESSES: ' . $row['bname'] . '</p>
+                <p>ADDRESS: ' . $row['baddr'] . '</p>
+                <input type="submit" name="accept" value="ACCEPT">
+                </div>';
+                    }
             } else {
                 echo "No Drivers Yet. Come back soon!";
-        }
+            }
             $conn->close();
             ?>
           
