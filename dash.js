91bfec1948a5e711987788c0ded3225e6f1662a9
William Thompson <williemaxt@gmail.com>
2018-04-11 02:01:08 -0400

    Update dash.js
    modal functions

--- dash.js
+++ dash.js
@@ -15,3 +15,31 @@ function myFunction() {
         navbar.classList.remove("sticky");
     }
 }
+
+//code for opening and closing the modal
+// Get the modal
+var modal = document.getElementById('infoModal');
+var filesModal = document.getElementById('filesModal');
+// Get the button that opens the modal
+var btn = document.getElementById('infoBtn');
+var filesBtn = document.getElementById('filesBtn');
+// Get the <span> element that closes the info modal
+var span = document.getElementsByClassName("close")[0];
+// Get the <span> element that closes the files modal
+var spanFiles = document.getElementsByClassName("closeFiles")[0];
+//displays the info modal
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+//displays the files modal
+filesBtn.onclick = function() {
+    filesModal.style.display = "block";
+}
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+//close when user clicks on spanFiles
+spanFiles.onclick = function() {
+    filesModal.style.display = "none";
+}
