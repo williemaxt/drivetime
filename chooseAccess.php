4301a9c7c5c1df83146482c61e78f5ed411e7919
William Thompson <williemaxt@gmail.com>
2018-04-11 12:24:29 -0400

    Add files via upload

---
+++ chooseAccess.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Drive Time</title>
+    <link rel="stylesheet" href="css/forms.css">
+</head>
+<body>
+<h1 class="quickHeader">Which Kind of User Are You?</h1>
+
+
+<div id="choiceDiv">
+    <!--Navigation menu-->
+    <a class="quickLink" id="back" href="index.php">&#8592; Return Home</a>
+    <a class="quickLink" href="login1.php">Driver</a><br>
+    <a class="quickLink" href="login.php">Client</a>
+</div>
+
+</body>
+<script type="text/javascript" src="http://code.jquery.com/jquery-1.11.3.min.js"></script>
+<script src="js/index.js"></script>
+</html>
