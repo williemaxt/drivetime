11ecf52387bd67e254d42a610a9d7036457d729f
William Thompson <williemaxt@gmail.com>
2018-04-11 01:59:55 -0400

    Update driverdash.php
    Updates to front end

--- driverdash.php
+++ driverdash.php
@@ -53,13 +53,116 @@
                 <p>NAME: ' . $row1['name'] . '</p>
                 <p>EMAIL: ' . $row1['email'] . '</p>
                 <p>PHONE: ' . $row1['number'] . '</p>
-                <p>STATE: ' . $row1['state'] . '</p>';
+                <p>STATE: ' . $row1['state'] . '</p>
+                <p>Medical: <a href="driverDocs/' . $row1['medical'] . '" target="_blank">View</a></p>
+                <p>Crash Rep: <a href="driverDocs/' . $row1['crash_report'] . '" target="_blank">View</a></p>
+                <img id="infoBtn" src="images/pencil.svg">
+                <img id="filesBtn" src="images/document.svg">';
             }else{
                 echo 'We cant seem to pull your info';
             }
             ?>
         </aside>
 
+            <!-- The Modal The Modal For Account Info-->
+            <div id="infoModal" class="modal">
+
+                <!-- Modal content -->
+                <div class="modal-content">
+                    <span class="close">&times;</span>
+
+                    <form id=" " method="post" action="<?php echo $_SERVER['PHP_SELF']?>" enctype="multipart/form-data">
+                        <h1>Update Account</h1>
+                        <p>Full Name</p>
+                        <input value="<?php echo $row1['name'] .'' ?>" type="text" minlength="3" name="name">
+                        <p>Phone</p>
+                        <input value="<?php echo $row1['number'] .'' ?>"type="number" name="number">
+                        <p>CDL #</p>
+                        <input value="<?php echo $row1['cdl'] .'' ?>"type="text" name="cdl">
+                        <p>Current City</p>
+                        <input value="<?php echo $row1['city'] .'' ?>"type="text" name="city">
+                        <p>Years of Experience</p>
+                        <input value="<?php echo $row1['experience'] .'' ?>"type="number" name="experience">
+                        <p>Current State</p>
+                        <select name="state" id="state">
+                            <option selected="selected">Select a State</option>
+                            <option value="AL">Alabama</option>
+                            <option value="AK">Alaska</option>
+                            <option value="AZ">Arizona</option>
+                            <option value="AR">Arkansas</option>
+                            <option value="CA">California</option>
+                            <option value="CO">Colorado</option>
+                            <option value="CT">Connecticut</option>
+                            <option value="DE">Delaware</option>
+                            <option value="DC">District Of Columbia</option>
+                            <option value="FL">Florida</option>
+                            <option value="GA">Georgia</option>
+                            <option value="HI">Hawaii</option>
+                            <option value="ID">Idaho</option>
+                            <option value="IL">Illinois</option>
+                            <option value="IN">Indiana</option>
+                            <option value="IA">Iowa</option>
+                            <option value="KS">Kansas</option>
+                            <option value="KY">Kentucky</option>
+                            <option value="LA">Louisiana</option>
+                            <option value="ME">Maine</option>
+                            <option value="MD">Maryland</option>
+                            <option value="MA">Massachusetts</option>
+                            <option value="MI">Michigan</option>
+                            <option value="MN">Minnesota</option>
+                            <option value="MS">Mississippi</option>
+                            <option value="MO">Missouri</option>
+                            <option value="MT">Montana</option>
+                            <option value="NE">Nebraska</option>
+                            <option value="NV">Nevada</option>
+                            <option value="NH">New Hampshire</option>
+                            <option value="NJ">New Jersey</option>
+                            <option value="NM">New Mexico</option>
+                            <option value="NY">New York</option>
+                            <option value="NC">North Carolina</option>
+                            <option value="ND">North Dakota</option>
+                            <option value="OH">Ohio</option>
+                            <option value="OK">Oklahoma</option>
+                            <option value="OR">Oregon</option>
+                            <option value="PA">Pennsylvania</option>
+                            <option value="RI">Rhode Island</option>
+                            <option value="SC">South Carolina</option>
+                            <option value="SD">South Dakota</option>
+                            <option value="TN">Tennessee</option>
+                            <option value="TX">Texas</option>
+                            <option value="UT">Utah</option>
+                            <option value="VT">Vermont</option>
+                            <option value="VA">Virginia</option>
+                            <option value="WA">Washington</option>
+                            <option value="WV">West Virginia</option>
+                            <option value="WI">Wisconsin</option>
+                            <option value="WY">Wyoming</option>
+                        </select>
+                        <input class="submitBtn" type="submit" name="submit" value="submit">
+                        <br>
+                    </form>
+                </div>
+            </div>
+            <!--End of modal-->
+            <!-- The Modal The Modal For Account Info-->
+            <div id="filesModal" class="modal">
+
+                <!-- Modal content -->
+                <div class="modal-content">
+                    <span class="closeFiles">&times;</span>
+
+                    <form id=" " method="post" action="<?php echo $_SERVER['PHP_SELF']?>" enctype="multipart/form-data">
+                        <h1>Update Field</h1>
+                        <p>Medical</p>
+                        <input type="file" name="medical">
+                        <p>Crash Report</p>
+                        <input type="file" name="crash_report">
+                        <input class="submitBtn" type="submit" name="submit" value="submit">
+                        <br>
+                    </form>
+                </div>
+            </div>
+            <!--End of modal-->
         <main>
         <h1>Your Requests</h1>
 
