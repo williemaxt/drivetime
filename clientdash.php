5c00a4cd9e07e303460fff6c929353aeeba9a19f
cmerchant1598 <cmerchant1598@gmail.com>
2018-04-15 17:32:12 -0400

    Update clientdash.php

--- clientdash.php
+++ clientdash.php
@@ -24,6 +24,7 @@ if(empty($_POST['keyword'])){
 $result = mysqli_query($conn, $sql);
 //variable for displaying the current users information
 $result1 = mysqli_query($conn, $sql1);
+
 //transactions in sql. records current user email, driver email, token, and a current timestamp (Y-m-d h:i:s).
 $driveremail = $_POST['driveremail'];
 $token = $_POST['token'];
@@ -31,73 +32,9 @@ $timestamp = $_POST['timestamp'];
 $timestamp = date("Y-m-d h:i:s");
 //inserting into sql.
 $sql = "INSERT INTO transactions (client_email, driver_email, transaction_token, timestamp) VALUES ('$username', '$driveremail', '$token','$timestamp');";
-require_once('vendor/stripe/stripe-php/init.php');
-require_once('vendor/stripe/stripe-php/lib/Stripe.php')
-?>
-<!-- The required Stripe lib -->
-<script type="text/javascript" src="https://js.stripe.com/v2/"></script>
-
-<!-- jQuery is used only for this example; it isn't required to use Stripe -->
-<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-
-<script type="text/javascript">
-    // This identifies your website in the createToken call below
-    Stripe.setPublishableKey('pk_test_SF7SVH5ZE1Q3snDwNfCrAoWg');
-    var stripeResponseHandler = function(status, response) {
-        var $form = $('#payment-form');
-        if (response.error) {
-            // Show the errors on the form
-            $form.find('.payment-errors').text(response.error.message);
-            $form.find('button').prop('disabled', false);
-        } else {
-            // token contains id, last4, and card type
-            var token = response.id;
-            // Insert the token into the form so it gets submitted to the server
-            $form.append($('<input type="hidden" name="stripeToken" />').val(token));
-            // and re-submit
-            $form.get(0).submit();
-        }
-    };
-    jQuery(function($) {
-        $('#payment-form').submit(function(e) {
-            var $form = $(this);
-            // Disable the submit button to prevent repeated clicks
-            $form.find('button').prop('disabled', true);
-            Stripe.card.createToken($form, stripeResponseHandler);
-            // Prevent the form from submitting with the default action
-            return false;
-        });
-    });
-</script>
-<?php
-
-\Stripe\Stripe::setApiKey("sk_test_9PyrNgAkYffqfvViseLNf4Sa");
-
-
-if(isset($_POST['stripeToken'])) {
-    $token = $_POST['stripeToken'];
-
-    echo $token;
-
-    $customer = \Stripe\Customer::create(array(
-            "source" => $token,
-            "description" => "Example customer")
-    );
-
-
-
-    echo "customer ID is ".$customer->id;
-
-    $newCustomer = $customer->id;
-
-    $con = new mysqli('localhost','root','root','drive_time');
-
-    $transaction_token = $con->real_escape_string($_POST['stripeToken']);
-    $con->query("INSERT INTO transactions (transaction_token) VALUES ('$transaction_token')");
-
-}
 
 ?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -159,40 +96,8 @@ if(isset($_POST['stripeToken'])) {
             <p>EXPERIENCE: ' . $row['experience'] . ' YEARS</p>
             <p>Medical: <a href="driverDocs/' . $row['medical'] . '" target="_blank">View</a></p>
             <p>Crash Rep: <a href="driverDocs/' . $row['crash_report'] . '" target="_blank">View</a></p>
-            <input type="submit" value="hire" name="hire">  <h1>Charge $10 with Stripe</h1>
+            <input type="submit" value="hire" name="hire">  
 
-  <form action="" method="POST" id="payment-form">
-    <span class="payment-errors"></span>
-
-    <div class="form-row">
-      <label>
-        <span>Card Number</span>
-        <input type="text" size="20" data-stripe="number"/>
-      </label>
-    </div>
-
-    <div class="form-row">
-      <label>
-        <span>CVC</span>
-        <input type="text" size="4" data-stripe="cvc"/>
-      </label>
-    </div>
-
-    <div class="form-row">
-      <label>
-        <span>Expiration (MM/YYYY)</span>
-        <input type="text" size="2" data-stripe="exp-month"/>
-      </label>
-      <span> / </span>
-      <input type="text" size="4" data-stripe="exp-year"/>
-    </div>
-
-    <button type="submit">Submit Payment</button>
-  </form>
-
- 
-</body>
-</html>
             </div>';
             }
         } else {
@@ -203,8 +108,5 @@ if(isset($_POST['stripeToken'])) {
     </main>
 </div>
 </body>
-
-</body>
-</html>
 <script src="js/dash.js"></script>
 </html>
