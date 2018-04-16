035e2da662ebb9bb9953ed10b11bc4ea4a4e4c30
cmerchant1598 <cmerchant1598@gmail.com>
2018-04-16 02:40:17 -0400

    Update AdminDash.php

--- AdminDash.php
+++ AdminDash.php
@@ -8,7 +8,13 @@ if(!isset($_SESSION))
     exit;
 }
 
-
+//Search bar logic
+if(empty($_POST['keyword'])){
+    $sql  = 'SELECT * FROM transactions';
+}elseif(isset($_POST['keyword'])){
+    $search = $_POST['keyword'];
+    $sql = "SELECT * FROM transactions WHERE transactions.business LIKE '$search' ";
+}
 ?>
 <!DOCTYPE html>
 <html>
@@ -29,30 +35,51 @@ if(!isset($_SESSION))
 
 <div class="wrapper">
     <aside>
-        <h1>My Account</h1>
-        <?php
-
-        ?>
+        <h1>Search</h1>
+        <!--Searchbar elements-->
+        <form class="searchBar" method="post" action="AdminDash.php">
+            <input type="text" name="keyword" placeholder="Search by buisness (verbatim)">
+            <button type="submit" name="submit"><i class="fa fa-search"></i></button>
+        </form>
+        <br>
     </aside>
     <!--Main content-->
     <main>
 
+            <h2>&nbsp; &nbsp; &nbsp; ID &nbsp; &nbsp; &nbsp; Client Email &nbsp; &nbsp; Client Name &nbsp;  &nbsp;  &nbsp;Business &nbsp; &nbsp; &nbsp; &nbsp; Driver &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;  Timestamp  </h2>
 
         <?php
-        $sql = "SELECT id, client_email, client_name, business, driver_email, timestamp FROM transactions";
+
+
         $result = $conn->query($sql);
 
+
+        $id = "SELECT id FROM transactions";
+
         if ($result->num_rows > 0) {
-            // output data of each row
+            // output data of each row in the database and displays it as a card
             while($row = $result->fetch_assoc()) {
-                echo "<br>". $row["id"]." ". $row["client_email"]. " " . $row["client_name"] . " " . $row["business"] . " " . $row["driver_email"] . " " . $row["timestamp"] . "<a href=\"delete.php?del=<?php echo $client_id ?>\"><button class=\"btn\">Delete</button></a><br>";
+                echo "<div class='admin-card'>
+                      <div class='admin-content'>
+                       <p>". $row["id"]." </p>
+                       <p>". $row["client_email"]."</p>
+                       <p>" . $row["client_name"] ." </p>
+                       <p>" . $row["business"] . "</p>
+                       <p>" . $row["driver_email"] ."</p>
+                       <p>" . $row["timestamp"] . "</p>
+                       </div>
+                       <a href='delete.php?del=<?php echo $id ?>'> <button type='button' name='button'>Delete</button></a>
+                       </div>
+                       <br>" ;
             }
         } else {
-            echo "0 results";
+            echo "No Transactions for $search Yet. Come back soon!";
         }
+
+
      //Will add code to pull from transactions sql db.
      //Show results as Client Email, Client Name, Business, Driver Email, Timestamp. Optionally Amount offered.
-
+        $conn->close();
      ?>
     </main>
 </div>
