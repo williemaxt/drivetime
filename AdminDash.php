bf8b7c8129e4ed49231df74acf8294bd3be1f135
cmerchant1598 <cmerchant1598@gmail.com>
2018-04-15 20:38:48 -0400

    Update AdminDash.php
    Rows["number"] is dependent upon sql table row order. For example, $row[0] would be id in my table cause it is in the first spot. $row[1] would be client_email... etc....

--- AdminDash.php
+++ AdminDash.php
@@ -8,14 +8,7 @@ if(!isset($_SESSION))
     exit;
 }
 
-$sql = 'SELECT * 
-		FROM transactions';
 
-$query = mysqli_query($conn, $sql);
-
-if (!$query) {
-    die ('SQL Error: ' . mysqli_error($conn));
-}
 ?>
 <!DOCTYPE html>
 <html>
@@ -38,17 +31,67 @@ if (!$query) {
     <aside>
         <h1>My Account</h1>
         <?php
-        //Admin Name, ProDriveTime, Edit Info. 
+
         ?>
     </aside>
     <!--Main content-->
     <main>
-        <h1>Transactions</h1>
-     <?php
-     
+        <h1 align="center">Transactions</h1>
+        <div class="table-scrol">
+
+
+            <div class="table-responsive"><!--this is used for responsive display in mobile and other devices-->
+
+
+                <table class="table table-bordered table-hover table-striped" style="table-layout: fixed">
+                    <thead>
+
+                    <tr>
+
+                        <th>User Id</th>
+                        <th>Client Email</th>
+                        <th>Client Name</th>
+                        <th>Business</th>
+                        <th>Driver Email</th>
+                        <th>Timestamp</th>
+                    </tr>
+                    </thead>
+
+                    <?php
+
+        $view_users_query="select * from transactions"; //select query for viewing users.
+        $run=mysqli_query($conn,$view_users_query); //run the sql query.
+
+        while($row=mysqli_fetch_array($run)) //while running query look to fetch the result and store in an array called $row.
+        {
+
+            $client_id = $row[0];
+            $client_email = $row[1];
+            $client_name = $row[2];
+            $business = $row[3];
+            $driver_email = $row[6];
+            $timestamp = $row[8];
+
+        }
+        ?>
+
+        <tr>
+            <!--here showing results in the table -->
+            <td><?php echo $client_id;  ?></td>
+            <td><?php echo $client_email;  ?></td>
+            <td><?php echo $client_name;  ?></td>
+            <td><?php echo $business;  ?></td>
+            <td><?php echo $driver_email;  ?></td>
+            <td><?php echo $timestamp;  ?></td>
+            <td><a href="delete.php?del=<?php echo $client_id ?>"><button class="btn">Delete</button></a></td> <!--btn btn-danger is a bootstrap button to show danger-->
+        </tr>
+
+
+        <?php
+
      //Will add code to pull from transactions sql db.
      //Show results as Client Email, Client Name, Business, Driver Email, Timestamp. Optionally Amount offered.
-     
+
      ?>
     </main>
 </div>
