7805918f8ca59521d15bae23b7d4ecc84fe05ee0
cmerchant1598 <cmerchant1598@gmail.com>
2018-05-04 14:35:15 -0400

    Create forgotPassword1.php
    forgotPassword1 for client

---
+++ forgotPassword1.php
@@ -0,0 +1,21 @@
+<?php
+session_start();
+$sessData = !empty($_SESSION['sessData'])?$_SESSION['sessData']:'';
+if(!empty($sessData['status']['msg'])){
+    $statusMsg = $sessData['status']['msg'];
+    $statusMsgType = $sessData['status']['type'];
+    unset($_SESSION['sessData']['status']);
+}
+?>
+    <h2>Enter the Email associated with your account to Reset Password</h2>
+<?php echo !empty($statusMsg)?'<p class="'.$statusMsgType.'">'.$statusMsg.'</p>':''; ?>
+    <div class="container">
+        <div class="regisFrm">
+            <form action="login.php" method="post">
+                <input type="email" autocomplete="off" name="email" placeholder="Email" required="">
+                <div class="send-button">
+                    <input type="submit" name="forgotSubmit1" value="Continue...">
+                </div>
+            </form>
+        </div>
+    </div>
