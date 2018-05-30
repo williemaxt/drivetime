c0ad1bc9ad45cf58b395fae35179716fc2e26b54
cmerchant1598 <cmerchant1598@gmail.com>
2018-05-30 16:07:31 -0400

    Update AdminDash.php

--- AdminDash.php
+++ AdminDash.php
@@ -1,93 +1,100 @@
 <?php
-session_start();
+
 include_once 'connection.php';
 
 if(!isset($_SESSION))
 {
-    header('Location:index.php');
-    exit;
+   header('Location:index.php');
+   exit;
 }
+
 //Search bar logic
 if(empty($_POST['keyword'])){
-    $sql  = 'SELECT * FROM transactions';
+   $sql  = 'SELECT * FROM transactions';
 }elseif(isset($_POST['keyword'])){
-    $search = $_POST['keyword'];
-    $sql = "SELECT * FROM transactions WHERE transactions.business LIKE '$search' ";
+   $search = $_POST['keyword'];
+   $sql = "SELECT * FROM transactions WHERE transactions.business LIKE '$search' ";
 }
 ?>
+
 <!DOCTYPE html>
 <html>
 <head>
-    <meta charset="utf-8">
-    <title>Drive Time</title>
-    <link rel="stylesheet" href="css/dash.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <meta charset="utf-8">
+   <title>Drive Time</title>
+   <link rel="stylesheet" href="css/dash.css">
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
-
 <nav id="navbar">
-    <h1>Prodrivetime</h1>
-    <p><a href="logout.php?logout=true">Logout</a></p>
+   <a href="#"><h1>Prodrivetime</h1></a>
+   <p><a href="logout.php?logout=true">Logout</a></p>
+   <p><a href="AdminDash.php">See Transactions</a></p>
+   <p><a href="DriverSheet.php">See Drivers</a></p>
+   <p><a href="ClientSheet.php">See Clients</a></p>
+   <p><a href="UpdateInfo.php">Manage Info</a></p>
+   <p><a href="UpdateHome.php">Manage Home</a></p>
 </nav>
 
 <body>
 
-
 <div class="wrapper">
-    <aside>
-        <h1>Search</h1>
-        <!--Searchbar elements-->
-        <form class="searchBar" method="post" action="AdminDash.php">
-            <input type="text" name="keyword" placeholder="Search by email">
-            <button type="submit" name="submit"><i class="fa fa-search"></i></button>
-        </form>
-        <br>
-    </aside>
-    <!--Main content-->
-    <main id="admin-main">
+   <aside>
+       <h1>Search</h1>
+       <!--Searchbar elements-->
+       <form class="searchBar" method="post" action="AdminDash.php">
+           <input type="text" name="keyword" placeholder="Search by email">
+           <button type="submit" name="submit"><i class="fa fa-search"></i></button>
+       </form>
+       <br>
+   </aside>
+   <!--Main content-->
+   <main id="admin-main">
 
-            <h2>&nbsp; &nbsp; &nbsp; Client Email &nbsp; &nbsp; Client Name &nbsp;  &nbsp;  &nbsp;Business &nbsp; &nbsp; &nbsp; &nbsp; Driver &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;  Timestamp  </h2>
+           <h2>&nbsp; &nbsp; &nbsp; Client Email &nbsp; &nbsp; Client Name &nbsp;  &nbsp;  &nbsp;Business &nbsp; &nbsp; &nbsp; &nbsp; Driver &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;  Timestamp  </h2>
 
-        <?php
+<?php
 
+       $result = $conn->query($sql);
 
-        $result = $conn->query($sql);
+       if ($result->num_rows > 0) {
+           // output data of each row in the database and displays it as a card
+           while($row = $result->fetch_assoc()) {
+               echo "
+                     <form method='post'>
+                     <div class='admin-card'>
+                     <div class='admin-content'>
+                      <p>". $row["client_email"]."</p>
+                      <p>" . $row["client_name"] ." </p>
+                      <p>" . $row["business"] . "</p>
+                      <p>" . $row["driver_email"] ."</p>
+                      <p>" . $row["timestamp"] . "</p>
+                      </div>
+                      <a href='' name='delete'><button type='submit' name='delete'>Delete</button></a>
+                     <input  name='id' type='hidden' value=" . $row["id"] . ">
+                     </form>
+                      </div>
+                      <br>" ;
+           }
+       } else {
+           echo "No Transactions Yet. Come back soon!";
+       }
 
+       if(isset($_POST['delete'])){
 
-        if ($result->num_rows > 0) {
-            // output data of each row in the database and displays it as a card
-            while($row = $result->fetch_assoc()) {
-                echo "
-                      <form method='post'>
-                      <div class='admin-card'>
-                      <div class='admin-content'>
-                       <p>". $row["client_email"]."</p>
-                       <p>" . $row["client_name"] ." </p>
-                       <p>" . $row["business"] . "</p>
-                       <p>" . $row["driver_email"] ."</p>
-                       <p>" . $row["timestamp"] . "</p>
-                       </div>
-                       <a href='' name='delete'><button type='submit' name='delete'>Delete</button></a>
-                      <input  name='id' type='hidden' value=" . $row["id"] . "> 
-                      </form>
-                       </div>
-                       <br>" ;
-            }
-        } else {
-            echo "No Transactions for $search Yet. Come back soon!";
-        }
+           $id = $_POST['id'];
 
-        if(isset($_POST['delete'])){
+           $conn->query("DELETE FROM transactions WHERE id='$id'");
+           echo "<meta http-equiv='refresh' content='0'>";
+           exit();
 
-            $id = $_POST['id'];
+       }
 
-            $conn->query("DELETE FROM transactions WHERE id='$id'");
-            echo "<meta http-equiv='refresh' content='0'>";
-            exit();
-
-        }
 ?>
-    </main>
+</main>
 </div>
 </body>
 <script src="js/dash.js"></script>
+<script src="js/index.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 </html>
