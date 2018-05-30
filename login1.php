db0e46e32cf42e8d8937e934a05f13bb33322596
cmerchant1598 <cmerchant1598@gmail.com>
2018-05-30 16:16:52 -0400

    Update login1.php
    updated 000

--- login1.php
+++ login1.php
@@ -1,15 +1,16 @@
 <?php
-session_start();
+
+include 'connection.php';
 
 include 'users.php';
 $user = new User();
 
 
 use PHPMailer\PHPMailer\PHPMailer;
-use phpmailer\Exception;
+use phpmailer\phpmailer\Exception;
 
 date_default_timezone_set('Etc/UTC');
-require 'vendor/autoload.php';
+include 'vendor/autoload.php';
 
 
 if(isset($_POST['forgotSubmit'])){
@@ -33,7 +34,11 @@ if(isset($_POST['forgotSubmit'])){
             $update = $user->update($data, $conditions);
 
             if($update){
-                $resetPassLink = 'http://localhost:8888/resetPassword.php?fp_code='.$uniqidStr;
+                $resetPassLink = 'https://drivetimepro1.000webhostapp.com/resetPassword.php?fp_code='.$uniqidStr;
+                //Above line will need editing. 'localhost:8888' must be removed and exchanged for your domain.
+                //Keep the /resetPassword.php?fp_code... This is very important.
+                //Example: https://www.prodrivetime.com/resetPassword.php?fp_code='.$uniqidStr;
+
 
                 //get user details
                 $con['where'] = array('email'=>$_POST['email']);
@@ -41,40 +46,51 @@ if(isset($_POST['forgotSubmit'])){
                 $userDetails = $user->getRows($con);
 
 
-
-
 date_default_timezone_set('Etc/UTC');
 //Create a new PHPMailer instance
 include_once 'forgotPassword.php';
 
+//This below portion for mail must be edited.
+
 $mail = new PHPMailer(true);
 try {
 //Tell PHPMailer to use SMTP
+// Do not touch this line below.
     $mail->isSMTP();
 //Enable SMTP debugging
 // 0 = off (for production use)
 // 1 = client messages
 // 2 = client and server messages
+// In your case this will need to be set at 0. If it is already zero you can skip this line.
     $mail->SMTPDebug = 0;
 //Set the hostname of the mail server
+//If you are using gmail, leave it as is.
     $mail->Host = 'smtp.gmail.com';
 //Set the SMTP port number - likely to be 25, 465 or 587
+//If you are using gmail, port 587 will suffice and you can leave this as is.
     $mail->Port = 587;
 //Whether to use SMTP authentication
+//Do not edit the line below.
     $mail->SMTPAuth = true;
 //Username to use for SMTP authentication
-    $mail->Username = 'email_username (ex. youremail@gmail.com');
+// Type in your email below in place of the example email placeholder. Make sure to leave the single quotes.
+    $mail->Username = 'drivetimedriver@gmail.com';
 //Password to use for SMTP authentication
-    $mail->Password = 'password';
+// Enter your password to the email account below. Make sure to leave the single quotes.
+    $mail->Password = 's3cur1ty!!!';
+    //Secure email transmissions
+    //$mail->SMTPSecure = 'ssl'; //tls or ssl.
 //Set who the message is to be sent from
-    $mail->setFrom('YOUREMAIL@email.com', 'ProDriveTime');
+//Set your email in the placeholder below. Leave the single quotes and make sure not to remove any code.
+//You can change the name that will show in the users email inbox. This should be a business tag. As in the example below.
+    $mail->setFrom('drivetimedriver@gmail.com', 'ProDriveTime');
 //Set an alternative reply-to address
-    $mail->addReplyTo('YOUREMAIL@email.com', 'ProDriveTime');
+//Repeat the same process as the last line of code. Email, business tag.
+    $mail->addReplyTo('drivetimedriver@gmail.com', 'ProDriveTime');
 //Set who the message is to be sent to
-    //(Leave this line intact but commented) $mail->addAddress('YOUREMAIL@gmail.com', 'John Doe');
+// Do not change the code below. There is no need to change anything beyond this point.
+//There is nothing else that needs to be done on this page.
     $mail->addAddress($_POST['email']);
-	// this above line is special. It will send email to whatever email typed in textbox. Autocomplete must remain off.
-	
 //Set the subject line
     $mail->Subject = 'PHPMailer SMTP test';
 //Read an HTML message body from an external file, convert referenced images to embedded,
@@ -102,7 +118,7 @@ try {
     //   }
     // }
 }
-
+                $sessData = array();
                 $sessData['status']['type'] = 'success';
                 $sessData['status']['msg'] = 'Please check your e-mail, we have sent a password reset link to your registered email.';
             }else{
@@ -122,7 +138,8 @@ try {
     $_SESSION['sessData'] = $sessData;
     //redirect to the forgot password page
     header("Location:forgotPassword.php");
-
+    exit;
+    
 }elseif(isset($_POST['resetSubmit'])){
     $fp_code = '';
     if(!empty($_POST['password']) && !empty($_POST['confirm_password']) && !empty($_POST['fp_code'])){
@@ -142,7 +159,7 @@ try {
                     'forgot_pass_identity' => $fp_code
                 );
                 $data = array(
-                    //'password' => md5($_POST['password'])
+
                     'password' => password_hash($_POST['password'] , PASSWORD_DEFAULT)
                 );
                 $update = $user->update($data, $conditions);
@@ -169,8 +186,6 @@ try {
     header("Location:".$redirectURL);
 }
 
-require_once("connection.php");
-
 if(isset($_POST['submit'])){
     $email = trim($_POST['email']);
     //this saves our email address in the session
@@ -193,9 +208,12 @@ if(isset($_POST['submit'])){
         echo "No User found";
     }
 }
+
+
 ?>
 <html>
 <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta charset="utf-8">
     <title>Driver Login</title>
     <link rel="stylesheet" href="css/forms.css">
