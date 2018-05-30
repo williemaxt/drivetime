12be4f0617b312a57e436331a46bc9debaf72e29
cmerchant1598 <cmerchant1598@gmail.com>
2018-05-30 16:11:19 -0400

    Update connection.php
    updated 000

--- connection.php
+++ connection.php
@@ -1,8 +1,12 @@
 <?php
-// this is where you pass in the info to conect to db
+session_start();
+
+// this is where you pass in the info to connect to db
+//type in your database credentials below. Servername, username, password, database_name.
 $dbServername = "localhost";
-$dbUsername = "root";
-$dbPassword = "ogbytheoz";
-$dbName = "drive_time";
+$dbUsername = "id5811675_root";
+$dbPassword = "s3cur1ty";
+$dbName = "id5811675_drive_time";
 //  this variable has the connection information
 $conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
+
