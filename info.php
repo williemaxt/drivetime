63fcbf00bbe776b25a256d220ffa6e63bb132104
cmerchant1598 <cmerchant1598@gmail.com>
2018-05-30 16:15:48 -0400

    Create info.php
    new 000

---
+++ info.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Drive Time</title>
+    <link rel="stylesheet" href="css/main.css">
+  </head>
+  <body>
+    <!--Navigation menu-->
+    <header>
+        <div id="burger" class="burger-nav">
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+      <nav id="navbar">
+          <h3 id="logoName">Prodrivetime</h3>
+        <ul>
+          <li><a href="index.php">Home</a></li>
+          <li><a href="clientRegister.php">Clients</a></li>
+          <li><a href="driverRegister.php">Drivers</a></li>
+          <li><a>Info</a></li>
+          <li><a href="chooseAccess.php">Login</a></li>
+        </ul>
+      </nav>
+    </header>
+    <!--Divider between the navigation and image-->
+    <div style="width:100%; height:5px; background:#ffffff;"></div>
+
+    <div id="imgHome"class="imgHeader">
+      <div class="imgText">
+        <h1>About Us</h1>
+        <p>This is where have all the info you need</p>
+      </div>
+
+    </div>
+    <!--Divider between the navigation and image-->
+    <div style="width:100%; height:5px; background:#ffffff;"></div>
+
+<div id="homeBar" class="bar">
+  <ul>
+    <li><p>Drive Today! >></p></li>
+    <li><a href="clientRegister.php"><button class="transparent-button">Sign Up</button></a></li>
+  </ul>
+</div>
+<br>
+<!--Main Content for page-->
+<main class="info">
+  <h1>About Us</h1>
+  <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+  <h1>Why Use Us?</h1>
+  <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+
+<div class="contact-card">
+  <ul>
+    <li><div class="contact-item">
+      <h1>Phone</h1>
+      <img src="images/call.svg" alt="">
+      <p>1234567890</p>
+      <br>
+    </div></li>
+
+    <li><div class="contact-item">
+      <h1>Email</h1>
+      <img src="images/close-envelope.svg" alt="">
+      <p>support@example.com</p>
+    </div></li>
+
+    <li><div class="contact-item">
+      <h1>Address</h1>
+      <img src="images/home.svg" alt="">
+      <p>123 Main Street, Anywhere, USA, 00000</p>
+    </div></li>
+
+  </div>
+  </ul>
+
+</main>
+<!--Divider between the navigation and image-->
+<div style="width:100%; height:5px; background:#ffffff;"></div>
+
+<footer>
+  <div class="wrapper">
+    <ul>
+      <li>
+        <h1>About Us</h1>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum repellat, maxime vel alias impedit veritatis temporibus, sequi quos veniam eius optio corporis modi dicta molestias at inventore culpa, natus explicabo.
+      </li>
+      <li>
+        <h1>Follow Us</h1>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum repellat, maxime vel alias impedit veritatis temporibus, sequi quos veniam eius optio corporis modi dicta molestias at inventore culpa, natus explicabo.
+      </li>
+      <li>
+        <h1>Follow Us</h1>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum repellat, maxime vel alias impedit veritatis temporibus, sequi quos veniam eius optio corporis modi dicta molestias at inventore culpa, natus explicabo.
+      </li>
+      <li>
+        <h1>Follow Us</h1>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum repellat, maxime vel alias impedit veritatis temporibus, sequi quos veniam eius optio corporis modi dicta molestias at inventore culpa, natus explicabo.
+      </li>
+    </ul>
+  </div>
+<div class="footerBottom">
+ <p>&copy; Drive Time 2018. All Rights Reserved. Developed By 611 Solutions, LLC</p>
+</div>
+</footer>
+  </body>
+  <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="js/index.js"></script>
+</html>
